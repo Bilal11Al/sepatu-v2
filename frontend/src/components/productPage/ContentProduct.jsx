8e2950e75314e3d954fe987125f029893bc23567
Bilal11Al <bilalaltintop28@gmail.com>
2026-04-16 10:30:10 +0700

    finish Product

--- frontend/src/components/productPage/ContentProduct.jsx
+++ frontend/src/components/productPage/ContentProduct.jsx
@@ -1,13 +1,72 @@
 export default function ContentProduct() {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
-            <div className="bg-blue-200 h-40 rounded-xl"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="bg-white rounded-2xl shadow-md border border-gray-100 overflow-hidden hover:shadow-xl transition-shadow duration-300">
+                <div className="p-2">
+                    <img
+                        src="https://i.ytimg.com/vi/zC0EZOOHifY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCvPH-EKThI2QS59oL-UcHM7kwLkQ"
+                        alt="Sneakers"
+                        className="rounded-xl w-full h-48 object-cover"
+                    />
+                </div>
+                <div className="px-4 pb-4">
+                    <p className="text-gray-800 text-lg font-bold truncate">Nike Air Jordan High</p>
+                    <span className="text-[10px] font-semibold">Casual</span>
+                    <div className="flex gap-2 my-2">
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">39</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">40</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">41</span>
+                    </div>
+                    <p className="text-blue-600 font-black text-xl mb-3">Rp 1.200.000</p>
+                    <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded-xl transition-colors active:scale-95 duration-150 cursor-pointer">
+                        Lihat Detail
+                    </button>
+                </div>
+            </div>
+            <div className="bg-white rounded-2xl shadow-md border border-gray-100 overflow-hidden hover:shadow-xl transition-shadow duration-300">
+                <div className="p-2">
+                    <img
+                        src="https://i.ytimg.com/vi/zC0EZOOHifY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCvPH-EKThI2QS59oL-UcHM7kwLkQ"
+                        alt="Sneakers"
+                        className="rounded-xl w-full h-48 object-cover"
+                    />
+                </div>
+                <div className="px-4 pb-4">
+                    <p className="text-gray-800 text-lg font-bold truncate">Nike Air Jordan High</p>
+                    <span className="text-[10px] font-semibold">Casual</span>
+                    <div className="flex gap-2 my-2">
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">39</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">40</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">41</span>
+                    </div>
+                    <p className="text-blue-600 font-black text-xl mb-3">Rp 1.200.000</p>
+                    <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded-xl transition-colors active:scale-95 duration-150 cursor-pointer">
+                        Lihat Detail
+                    </button>
+                </div>
+            </div>
+            <div className="bg-white rounded-2xl shadow-md border border-gray-100 overflow-hidden hover:shadow-xl transition-shadow duration-300">
+                <div className="p-2">
+                    <img
+                        src="https://i.ytimg.com/vi/zC0EZOOHifY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCvPH-EKThI2QS59oL-UcHM7kwLkQ"
+                        alt="Sneakers"
+                        className="rounded-xl w-full h-48 object-cover"
+                    />
+                </div>
+                <div className="px-4 pb-4">
+                    <p className="text-gray-800 text-lg font-bold truncate">Nike Air Jordan High</p>
+                    <span className="text-[10px] font-semibold">Casual</span>
+                    <div className="flex gap-2 my-2">
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">39</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">40</span>
+                        <span className="text-[10px] bg-gray-100 px-2 py-1 rounded text-gray-600 font-semibold">41</span>
+                    </div>
+                    <p className="text-blue-600 font-black text-xl mb-3">Rp 1.200.000</p>
+                    <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded-xl transition-colors active:scale-95 duration-150 cursor-pointer">
+                        Lihat Detail
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/components/productPage/FillterProducts.jsx
+++ frontend/src/components/productPage/FillterProducts.jsx
@@ -1,7 +1,4 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline"
-import { useState } from "react"
 export default function FillterProducts() {
-    const [open, setOpen] = useState(false);
     return (
         <div className="w-full md:w-72 bg-white border border-gray-100 shadow-sm rounded-3xl p-6  top-5">
             <h3 className="font-black text-2xl mb-6 text-gray-800 tracking-tight">Katalog</h3>
@@ -25,63 +22,53 @@ export default function FillterProducts() {
             <div className="pt-6 border-t border-gray-100">
                 <p className="font-bold text-lg uppercase tracking-widest text-blue-600">Filter by:</p>
                 <div
-                    className="flex justify-between items-center cursor-pointer group select-none"
-                    onClick={() => setOpen(prev => prev === "size" ? null : 'size')}
-                >
+                    className="flex justify-between items-center cursor-pointer group select-none">
                     <p className="font-bold text-xs uppercase tracking-widest text-blue-600"> Size</p>
-                    <ChevronDownIcon className={`h-4 text-gray-400 transition-transform duration-300 ${open === "size" ? "rotate-180 text-blue-600" : ''}`} />
                 </div>
-                {open === "size" && (
-                    <div className="mt-4 grid grid-cols-2 gap-2">
-                        <label className="flex items-center justify-center border border-gray-200 rounded-xl py-2 cursor-pointer hover:border-blue-500 hover:bg-blue-50 transition-all group">
-                            <input type="checkbox" className="hidden" />
-                            <span className="text-sm font-bold text-gray-600 group-hover:text-blue-600">39</span>
-                        </label>
-                        <label className="flex items-center justify-center border border-gray-200 rounded-xl py-2 cursor-pointer hover:border-blue-500 hover:bg-blue-50 transition-all group">
-                            <input type="checkbox" className="hidden" />
-                            <span className="text-sm font-bold text-gray-600 group-hover:text-blue-600">40</span>
-                        </label>
-                        <label className="flex items-center justify-center border border-gray-200 rounded-xl py-2 cursor-pointer hover:border-blue-500 hover:bg-blue-50 transition-all group">
-                            <input type="checkbox" className="hidden" />
-                            <span className="text-sm font-bold text-gray-600 group-hover:text-blue-600">41</span>
-                        </label>
-
-                        <label className="flex items-center justify-center border border-gray-200 rounded-xl py-2 cursor-pointer hover:border-blue-500 hover:bg-blue-50 transition-all group">
-                            <input type="checkbox" className="hidden" />
-                            <span className="text-sm font-bold text-gray-600 group-hover:text-blue-600">42</span>
-                        </label>
-                    </div>
-                )}
+                <div className="mt-4 ">
+                    <label className="flex items-center gap-2  ">
+                        <input type="checkbox" className="w-4 h-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                        <span className="text-sm font-bold text-gray-600 ">39</span>
+                    </label>
+                    <label className="flex items-center gap-2 ">
+                        <input type="checkbox" className="w-4 h-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                        <span className="text-sm font-bold text-gray-600 ">40</span>
+                    </label>
+                    <label className="flex items-center  gap-2">
+                        <input type="checkbox" className="w-4 h-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                        <span className="text-sm font-bold text-gray-600 ">41</span>
+                    </label>
+                    <label className="flex items-center  gap-2">
+                        <input type="checkbox" className="w-4 h-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                        <span className="text-sm font-bold text-gray-600 ">42</span>
+                    </label>
+                </div>
 
                 <div
-                    className="flex justify-between items-center cursor-pointer group select-none mt-2"
-                    onClick={() => setOpen(prev => prev === "price" ? null : 'price')}
-                >
+                    className="flex justify-between items-center cursor-pointer group select-none mt-2">
                     <p className="font-bold text-xs uppercase tracking-widest text-blue-600">Price</p>
-                    <ChevronDownIcon className={`h-4 text-gray-400 transition-transform duration-300 ${open === "price" ? "rotate-180 text-blue-600" : ''}`} />
                 </div>
-                {open === "price" && (
-                    <>
-                        <div className="p-5">
-                            <label className="flex justify-between items-center gap-2 mb-2">
-                                <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
-                                <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
-                            </label>
-                            <label className="flex justify-between items-center gap-2 mb-2">
-                                <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
-                                <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
-                            </label>
-                            <label className="flex justify-between items-center gap-2 mb-2">
-                                <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
-                                <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
-                            </label>
-                            <label className="flex justify-between items-center gap-2 mb-2">
-                                <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
-                                <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
-                            </label>
-                        </div>
-                    </>
-                )}
+                <div className="mt-4">
+                    <label className="flex justify-between items-center gap-2 mb-2">
+                        <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
+                        <span className="text-sm font-bold ">Rp.150.000 - Rp.250.000</span>
+                    </label>
+                    <label className="flex justify-between items-center gap-2 mb-2">
+                        <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
+                        <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
+                    </label>
+                    <label className="flex justify-between items-center gap-2 mb-2">
+                        <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
+                        <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
+                    </label>
+                    <label className="flex justify-between items-center gap-2 mb-2">
+                        <input type="checkbox" className="w-4 h-4 accent-blue-600 cursor-pointer" />
+                        <span className="text-sm font-bold">Rp.150.000 - Rp.250.000</span>
+                    </label>
+                </div>
+            </div>
+            <div className="flex justify-end items-center mt-5">
+                <button className="bg-blue-600 p-1 px-10 text-white rounded-sm cursor-pointer">Cari</button>
             </div>
         </div>
     )
