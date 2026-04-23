11c34608d608de553bfebbddbaa5fa3f79f226af
Bilal11Al <bilalaltintop28@gmail.com>
2026-04-23 11:07:13 +0700

    done contact

--- frontend/src/components/contactPage/ContactMe.jsx
+++ frontend/src/components/contactPage/ContactMe.jsx
@@ -1,82 +1,83 @@
-export default function ContactMe () {
-    return(
-         <div className="max-w-5xl mx-auto my-10 shadow-2xl ">
-                <div className="grid grid-cols-1 md:grid-cols-2  ">
-                    <div className="bg-gray-300 p-2 px-5 py-10 ">
-                        <h3 className="text-black text-3xl font-extrabold">Get in touch</h3>
-                        <p className="text-black text-sm mt-5 f">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                        <div className="flex items-center gap-1 px-5 mt-5">
-                            <div className="bg-amber-900 rounded-full p-1">
-                                <h1>logo</h1>
-                            </div>
-                            <div className="bg-amber-300">
-                                <p>Get in touch</p>
-                                <span>Lorem Ipsumis simply dummy </span>
-                            </div>
+import { FaInstagram, FaTiktok, FaFacebook } from "react-icons/fa";
+export default function ContactMe() {
+    return (
+        <div className="max-w-5xl mx-auto my-10 shadow-2xl ">
+            <div className="grid grid-cols-1 md:grid-cols-2  ">
+                <div className="bg-gray-300 p-2 px-5 py-10 ">
+                    <h3 className="text-black text-3xl font-extrabold">Get in touch</h3>
+                    <p className="text-black text-sm mt-5 f">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <div className="flex items-center gap-1 px-5 mt-5">
+                        <div className="rounded-full p-1">
+                            <FaInstagram className="w-8 h-8 text-black" />
                         </div>
-                        <div className="flex items-center gap-1 px-5 mt-5">
-                            <div className="bg-amber-900 rounded-full p-1">
-                                <h1>logo</h1>
-                            </div>
-                            <div className="bg-amber-300">
-                                <p>Get in touch</p>
-                                <span>Lorem Ipsumis simply dummy </span>
-                            </div>
-                        </div>
-                        <div className="flex items-center gap-1 px-5 mt-5">
-                            <div className="bg-amber-900 rounded-full p-1">
-                                <h1>logo</h1>
-                            </div>
-                            <div className="bg-amber-300">
-                                <p>Get in touch</p>
-                                <span>Lorem Ipsumis simply dummy </span>
-                            </div>
-                        </div>
-                        <div className="mt-5">
-                            <p className="px-5 text-lg font-semibold">Follow me</p>
-                            <div className="flex items-center px-5 mt-2 gap-1">
-                                <div className="bg-black p-1 rounded-full"><p>logo</p></div>
-                                <div className="bg-black p-1 rounded-full"><p>logo</p></div>
-                                <div className="bg-black p-1 rounded-full"><p>logo</p></div>
-                            </div>
+                        <div className="bg-amber-300">
+                            <p>Get in touch</p>
+                            <span>Lorem Ipsumis simply dummy </span>
                         </div>
                     </div>
-                    <div className="p-1 shadow-2xl ">
-                        <p className="px-5 py-5 font-semibold text-lg">Send us message</p>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-                            <div className="px-5 flex flex-col">
-                                <label htmlFor="" className="text-sm font-semibold">Name</label>
-                                <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
-                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
-                            </div>
-                            <div className="px-5  flex flex-col">
-                                <label htmlFor="" className="text-sm font-semibold ">Name</label>
-                                <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
-                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
-                            </div>
+                    <div className="flex items-center gap-1 px-5 mt-5">
+                        <div className="rounded-full p-1">
+                            <FaInstagram className="w-8 h-8 text-black" />
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 mt-5">
-                            <div className="px-5 flex flex-col">
-                                <label htmlFor="" className="text-sm font-semibold">Name</label>
-                                <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
-                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
-                            </div>
-                            <div className="px-5  flex flex-col">
-                                <label htmlFor="" className="text-sm font-semibold ">Name</label>
-                                <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
-                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
-                            </div>
+                        <div className="bg-amber-300">
+                            <p>Get in touch</p>
+                            <span>Lorem Ipsumis simply dummy </span>
                         </div>
-                        <div className="grid grid-cols-1  p-1 mt-5 px-5">
-                            <label htmlFor="" className="text-sm font-semibold">Deskrpsi</label>
-                            <textarea name="" id="" className="outline-none bg-white  border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
-                       rounded-xl shadow-sm transition-all duration-300 text-sm"></textarea>
+                    </div>
+                    <div className="flex items-center gap-1 px-5 mt-5">
+                        <div className="rounded-full p-1">
+                            <FaInstagram className="w-8 h-8 text-black" />
                         </div>
-                        <div className="flex justify-center items-center mt-2 px-5 mb-5">
-                            <button className="bg-blue-500 rounded-sm cursor-pointer p-1 w-screen text-white font-bold hover:bg-blue-400">Kirim</button>
+                        <div className="bg-amber-300">
+                            <p>Get in touch</p>
+                            <span>Lorem Ipsumis simply dummy </span>
+                        </div>
+                    </div>
+                    <div className="mt-5">
+                        <p className="px-5 text-lg font-semibold">Follow me</p>
+                        <div className="flex items-center px-5 mt-2 gap-1">
+                            <div className="bg-black p-2 rounded-full hover:bg-blue-400"><FaInstagram className="w-6 h-6 text-white" /></div>
+                            <div className="bg-black p-2 rounded-full hover:bg-blue-400"><FaTiktok className="w-6 h-6 text-white" /></div>
+                            <div className="bg-black p-2 rounded-full hover:bg-blue-400"><FaFacebook className="w-6 h-6 text-white" /></div>
                         </div>
                     </div>
                 </div>
+                <div className="p-1 shadow-2xl ">
+                    <p className="px-5 py-5 font-semibold text-lg">Send us message</p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+                        <div className="px-5 flex flex-col">
+                            <label htmlFor="" className="text-sm font-semibold">Name</label>
+                            <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
+                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
+                        </div>
+                        <div className="px-5  flex flex-col">
+                            <label htmlFor="" className="text-sm font-semibold ">Name</label>
+                            <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
+                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-1 mt-5">
+                        <div className="px-5 flex flex-col">
+                            <label htmlFor="" className="text-sm font-semibold">Name</label>
+                            <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
+                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
+                        </div>
+                        <div className="px-5  flex flex-col">
+                            <label htmlFor="" className="text-sm font-semibold ">Name</label>
+                            <input type="text" className="outline-none bg-white px-5 border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
+                       rounded-xl shadow-sm transition-all duration-300 text-sm" />
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1  p-1 mt-5 px-5">
+                        <label htmlFor="" className="text-sm font-semibold">Deskrpsi</label>
+                        <textarea name="" id="" className="outline-none bg-white  border-2 border-gray-300  p-1 focus:border-blue-500 focus:ring-4 focus:ring-blue-50/50 
+                       rounded-xl shadow-sm transition-all duration-300 text-sm"></textarea>
+                    </div>
+                    <div className="flex justify-center items-center mt-2 px-5 mb-5">
+                        <button className="bg-blue-500 rounded-sm cursor-pointer p-1 w-screen text-white font-bold hover:bg-blue-400">Kirim</button>
+                    </div>
+                </div>
             </div>
+        </div>
     )
 }
--- frontend/src/pages/user/Contact.jsx
+++ frontend/src/pages/user/Contact.jsx
@@ -6,6 +6,20 @@ export default function Contact() {
         <div>
             <HeroContact />
             <ContactMe />
+            <div className="w-full max-w-8xl mx-auto overflow-hidden rounded-2xl shadow-lg border border-gray-200 mb-2">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31726.62339616773!2d106.9004475!3d-6.286336599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1776916014618!5m2!1sid!2sid"
+                    width="600"
+                    className="w-full h-96"
+                    height="450"
+                    style={{ border: 0 }}
+                    frameBorder="0"
+                    tabIndex="0"
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
         </div>
     )
 } 
